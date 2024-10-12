90afa9ce5d34f0f6b8224be13a5ae3163ada69df
David Smith <dave@rabbit.beer>
2024-10-11 20:02:38 -0400

    updated CSRF_TRUSTED_ORIGINS

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -148,4 +148,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True   #<-- added, but this will allow all websites to make requests(only good for dev)
 
-CSRF_TRUSTED_ORIGINS = ["https://crimetea.com", "https://*.crimetea.com", "https://api.crimetea.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://crimetea.com",
+    "https://*.crimetea.com",
+    "https://api.crimetea.com",
+    "http://127.0.0.1:8000/api/books",
+    "http://127.0.0.1:8000/api",    
+    "http://127.0.0.1:8000",
+]
