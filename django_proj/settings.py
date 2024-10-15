f94b24ef23d9b8348b944600edc3077767bc3a0b
David Smith <dave@rabbit.beer>
2024-10-15 12:28:53 -0400

    debug false

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k0qidreq)mr%e6_!__)qi6%p=edpzsy%-@tfmx42g+dyidkj$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']   #<-- MAY BE NEEDED FOR EC2 HOSTING
 
